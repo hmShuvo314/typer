1d25cb4a9dfeecd5cd11535394b21b88e197f272
hmshuvo314 <hmshuvo314@gmail.com>
2022-04-04 19:14:53 +0600

    Final Touch

--- client/src/lib/GenerateTestSentence.js
+++ client/src/lib/GenerateTestSentence.js
@@ -3,7 +3,9 @@ import { words } from "./words";
 export const generateTestSentence = () => {
   const randomSentenceLength = Math.floor(Math.random() * 6 + 11);
 
-  const randomWordIdx = Math.floor(Math.random() * words.length);
+  const randomWordIdx = Math.floor(
+    Math.random() * (words.length - randomSentenceLength)
+  );
   const randomSentence = words.slice(
     randomWordIdx,
     randomWordIdx + randomSentenceLength
